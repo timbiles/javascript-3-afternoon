abaa2fedf7bf04ccab5ba40eb2ce716446fc0310
Tim Biles <timbilestimbiles@gmail.com>
2018-06-20 13:59:47 -0500

    halfway

--- classes.js
+++ classes.js
@@ -29,7 +29,20 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;    
+    this.age = age;
+  }
+
+  makeWidget(){
+    return this.first_name + ' ' + this.last_name + ' Widget'
+  }
+}
+
+Employee
 
 
 
@@ -49,7 +62,21 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports){  
+    super(first_name, last_name, email, age)
+    this.reports = [];
+  }
+  hire(newEmployee){
+    this.reports.push(newEmployee);
+  }
+  fire(index){
+    this.reports.splice(index, 1);
+  }
+}
+
+
+
 
 
 
@@ -75,7 +102,13 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports, title, bonus){  
+    super(first_name, last_name, email, age, reports)
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  }
+}
 
 
 
